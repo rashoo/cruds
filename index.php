e8aedd97fb60d7eaedcaa58db72b107b02c4f5ee
Raju Kumar <rakumar@csumb.edu>
2021-10-28 15:43:16 -0700

    Initial commit

--- index.php
+++ index.php
@@ -1,21 +1,64 @@
 <?php
+    require_once "database.php";
+
+    $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+    $statement->execute();
+    $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link href="app.css" rel="stylesheet"/>
+    <title>Products CRUD</title>
 </head>
 <body>
-<nav class="header-links">
-    <ul>
-        <a href="index.php">Home</a>
-        <a href="about.php">About Me</a>
-        <a href="about_project.php">My Projects</a>
-    </ul>
-</nav>
-<br><br>
+<h1>Products CRUD</h1>
+
+<p>
+    <a href="create.php" type="button" class="btn btn-sm btn-success">Add Product</a>
+</p>
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">Image</th>
+        <th scope="col">Title</th>
+        <th scope="col">Price</th>
+        <th scope="col">Create Date</th>
+        <th scope="col">Actions</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($products as $i => $product) { ?>
+        <tr>
+            <th scope="row"><?php echo $i + 1 ?></th>
+            <td>
+                <?php if ($product['image']): ?>
+                    <img src="<?php echo $product['image'] ?>" alt="<?php echo $product['title'] ?>" class="product-img">
+                <?php endif; ?>
+            </td>
+            <td><?php echo $product['title'] ?></td>
+            <td><?php echo $product['price'] ?></td>
+            <td><?php echo $product['create_date'] ?></td>
+            <td>
+                <a href="update.php?id=<?php echo $product['id'] ?>" class="btn btn-sm btn-outline-primary">Edit</a>
+                <form method="post" action="delete.php" style="display: inline-block">
+                    <input  type="hidden" name="id" value="<?php echo $product['id'] ?>"/>
+                    <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+    <?php } ?>
+    </tbody>
+</table>
+
+</body>
+</html>
